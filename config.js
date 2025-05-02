63bed2480a5d4dca48a91c57366f59c63c499062
SwineFluFlew <punchmychoda@gmail.com>
2025-05-02 09:26:43 -0400

    Initial push

---
+++ config.js
@@ -0,0 +1,28 @@
+const DEFAULT_KEYWORDS = [
+  "token alert",
+  "quick buy",
+  "gmgn.ai",
+  "24h change",
+  "wallet",
+  "pump",
+  "signal",
+  "GemPump",
+  "t.me/",
+  "💎",
+  "called in my",
+  "Memefast",
+  "spot early",
+  "giving away",
+  "giveaway", 
+  "private tg",
+  "first alert", 
+  "tg frens",
+  "Token:",
+  "my telegram",
+  "💎 Discover"
+];
+const DEFAULT_THRESHOLD = 2;
+
+// only run on “search” pages with addr/ticker
+const DEFAULT_FILTER_SEARCH_ONLY = false;
+
